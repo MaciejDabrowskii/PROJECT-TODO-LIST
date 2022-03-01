e69b3f0a96e1d3f320022d891193bb493890f1f5
Jimmy <dabrowski.mac.1@gmail.com>
2022-03-01 20:11:29 +0100

    working on functions

--- src/dom-functions/DOM-show-projects.js
+++ src/dom-functions/DOM-show-projects.js
@@ -6,18 +6,54 @@ let showProjects = () => {
         let projectDiv = document.createElement('div');
         projectDiv.classList.add('projectDiv');
         projectDiv.innerText = `${project.getName()}`;
+ 
+        const dropdown = document.createElement('div');
+        dropdown.classList.add('dropdown');
         
         const editProjectButton = document.createElement('button');
         editProjectButton.classList.add('editProjectButton');
-        editProjectButton.textContent = 'E'
+        editProjectButton.textContent = 'E';
+
+        const dropdownContent = document.createElement('div');
+        dropdownContent.classList.add('dropdownContent');
+
+        const editProjectNameInput = document.createElement('input');
+        editProjectNameInput.type = "text";
+        editProjectNameInput.id = "editProjectName";
+        
+        const editProjectNameLabel = document.createElement('Label');
+        editProjectNameLabel.setAttribute("for","editProjectName");
+        editProjectNameLabel.innerHTML = "New project name:";
+
+        const confirmNewNameButton = document.createElement('button');
+        confirmNewNameButton.classList.add('confirmNewNameButton');
+        confirmNewNameButton.textContent = 'OK';
+
+        projectButtonsEvents().editProjectButton(editProjectButton, dropdownContent, confirmNewNameButton, editProjectNameInput, project);
+
+        dropdownContent.append(editProjectNameLabel, editProjectNameInput, confirmNewNameButton);
+        dropdown.append(editProjectButton, dropdownContent);
+
+        
+        // const editProjectButton = (button, dropdown, confirm, input, project) => {
+        //     button.addEventListener('click', () =>{
+        //         dropdown.classList.toggle("show");
+        //         confirm.addEventListener('click', () =>{
+        //             project.editProjectName(input.value)
+        //         })
+        //         showProjects();
+        //     })
+        // };
+
         
         const deleteProjectButton = document.createElement('button');
         deleteProjectButton.classList.add('deleteProjectButton');
         deleteProjectButton.textContent = 'X';
+        projectButtonsEvents().deleteProjectButton(deleteProjectButton, index);
 
-        projectDiv.append(editProjectButton, deleteProjectButton);
+
+        projectDiv.append(dropdown, deleteProjectButton);
         document.querySelector('.projectsContainer').append(projectDiv);
-        projectButtonsEvents().deleteProjectButton(index);
     })
 }; 
 
--- src/functions/project-buttons-events.js
+++ src/functions/project-buttons-events.js
@@ -1,20 +1,30 @@
 import projectMenager from "../functions/project-menager.js";
 import showProjects from "../dom-functions/DOM-show-projects.js";
 const projectButtonsEvents = () => {
-    // const editProjectButton = () => {
-    //     const editProjectName = document.querySelector('.editProjectName');
-    //     editProjectName.addEventListener('click', () =>{
-    //         projectMenager.removeProject(index)
-    //     })
-    // };
-    const deleteProjectButton = (index) => {
-        const deleteProjectButton = document.querySelector('.deleteProjectButton');
-        deleteProjectButton.addEventListener('click', () =>{
+    const editProjectButton = (button, dropdown, confirm, input, project) => {
+        button.addEventListener('click', () =>{
+            dropdown.classList.toggle("show");
+            // window.onclick = function(event) {
+            //         !event.target.matches(dropdown) || 
+            //         !event.target.matches(button) || 
+            //         !event.target.matches(confirm) || 
+            //         !event.target.matches(button) 
+            //         ? dropdown.classList.toggle("show");
+            // }
+            confirm.addEventListener('click', () =>{
+                project.editProjectName(input.value);
+                showProjects();
+            });
+        })
+    };
+    const deleteProjectButton = (button, index) => {
+        button.addEventListener('click', () =>{
             projectMenager.removeProject(index);
             showProjects();
         });
     };
     return {
+        editProjectButton,
         deleteProjectButton
     }
 };
