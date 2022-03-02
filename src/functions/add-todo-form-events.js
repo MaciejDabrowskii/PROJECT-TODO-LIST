fdafdcf63eb70d92d628c9002dfcf384fa6fd1b1
Jimmy <dabrowski.mac.1@gmail.com>
2022-03-02 14:48:27 +0100

    working on functions

---
+++ src/functions/add-todo-form-events.js
@@ -0,0 +1,5 @@
+import projectMenager from "./project-menager.js";
+import projectCreator from "./project-creator.js";
+import todoTask from "./TODO-creator.js";
+import createAddTODOtBtn from "../dom-functions/DOM-addTodo-create-addBtn.js";
+import addTODOButtonEvent from "./add-todo-button-event.js";
