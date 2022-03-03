e78a067819cd684616e14653caf30214459dcd8c
Jimmy <dabrowski.mac.1@gmail.com>
2022-03-04 00:25:51 +0100

    working on functions

--- src/functions/add-project-divs-event.js
+++ src/functions/add-project-divs-event.js
@@ -10,22 +10,17 @@ const addProjectDivEvent = (project, index) => {
 
     const homeProject = document.querySelector('.homeProjectDiv');
     homeProject.classList.add('target');
-
-    project.addEventListener('click', () =>{
-        let projectsArray = [...document.querySelectorAll('.projectDiv')];
-        projectsArray.forEach((item) => {
-            item.classList.remove('target'); 
-        });
-        homeProject.classList.remove('target');
-        project.classList.add('target');
-        projectsArray
-
-        if (projectMenager.getProjectsArray().length === 0){
-            return
-        }else {
-            
+    const deleteProjectButton = document.querySelector('.deleteProjectButton');
+    project.addEventListener('click', (event) =>{
+        if (event.target === !deleteProjectButton) {
+            let projectsArray = [...document.querySelectorAll('.projectDiv')];
+            document.querySelector('.todosContainer').innerHTML = "";
+            projectsArray.forEach((item) => {
+                item.classList.remove('target'); 
+            });
+            homeProject.classList.remove('target');
+            project.classList.add('target');
             projectMenager.getProjectsArray()[index].getTodosArray().forEach((todo) => {
-                document.querySelector('.todosContainer').innerHTML = "";
                 domShowTodo(todo);
             });
         };
