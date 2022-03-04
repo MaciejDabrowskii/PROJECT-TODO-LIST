744384783f4bd96914ccd0481b68ac3d9aab3eb2
Jimmy <dabrowski.mac.1@gmail.com>
2022-03-04 10:06:31 +0100

    working on functions

--- src/functions/project-buttons-events.js
+++ src/functions/project-buttons-events.js
@@ -1,29 +1,32 @@
 import projectMenager from "../functions/project-menager.js";
 import showProjects from "../dom-functions/DOM-show-projects.js";
 const projectButtonsEvents = () => {
-    const editProjectButton = (button, dropdown, confirm, input, project) => {
-        button.addEventListener('click', () =>{
-            dropdown.classList.toggle("show");
-            confirm.addEventListener('click', () =>{
-                project.editProjectName(input.value);
-                showProjects();
+    const editProjectButton = (editButton, dropdown, confirm, input, project, deleteButton) => {
+        
+            editButton.addEventListener('click', () =>{
+                dropdown.classList.toggle("show");
+                confirm.addEventListener('click', () =>{
+                    project.editProjectName(input.value);
+                    showProjects();
+                });
             });
-        });
-        window.onclick = function(event) {
-            if (
-                (!event.target.matches('.editProjectButton')) &&
-                (!event.target.matches('.dropdownContent')) &&
-                (!event.target.matches('#editProjectName')) &&
-                (!event.target.matches('label')) &&
-                (!event.target.matches('.confirmNewNameButton'))
-               ) {
-                input.innerHTML = "";
-                const dropdown = document.querySelector(".dropdownContent");
-                if (dropdown.classList.contains('show')) {
-                    dropdown.classList.remove('show');
-                };
+            window.onclick = function(event) {
+                if (event.target === !document.querySelector('.deleteProjectButton')) {    
+                    if (
+                        (!event.target.matches('.editProjectButton')) &&
+                        (!event.target.matches('.dropdownContent')) &&
+                        (!event.target.matches('#editProjectName')) &&
+                        (!event.target.matches('label')) &&
+                        (!event.target.matches('.confirmNewNameButton'))
+                       ) {
+                        input.innerHTML = "";
+                        const dropdown = document.querySelector(".dropdownContent");
+                        if (dropdown.classList.contains('show')) {
+                            dropdown.classList.remove('show');
+                        };
+                    };
+                };    
             }
-        }
         
     };
     const deleteProjectButton = (button, index) => {
